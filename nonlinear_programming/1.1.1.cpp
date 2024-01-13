4d19efda59889e744f8a2b027b188456754f1813
root <root@localhost.localdomain>
2024-01-12 20:41:54 -0800

    recent

---
+++ nonlinear_programming/1.1.1.cpp
@@ -0,0 +1,10 @@
+Question 1.1.1
+
+Function: f(X,Y) = X*X + Y*Y + b*X*Y + X + 2*Y
+
+Stationary Points (b): df(X,Y)/dX = 2*X + b*Y + 1 = 0; b = -(2*X+1)/Y
+                       df(X,Y)/dY = 2*Y + b*Y + 2 = 0; b = -(2*Y+2)/X
+
+Rule: Unconstrained Optimization
+
+
