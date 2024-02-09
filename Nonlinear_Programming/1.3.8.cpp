462b1ca3dda6e32d97f3aaafc7ea7ff4f2594227
root <root@localhost.localdomain>
2024-02-08 20:59:04 -0800

    1.3.8.cpp

--- Nonlinear_Programming/1.3.8.cpp
+++ Nonlinear_Programming/1.3.8.cpp
@@ -62,61 +62,104 @@ c) Function #1: f(x)=0.5*x^2(1+gamma*cos(x))
 
 #include <iostream>
 #include <cmath>
-#include <chronos>
+#include <chrono>
+#include <vector>
+#include <functional>
+#include <limits>
 
 using namespace std;
 
 std::function<double(double, double)> RealFunc1;
-typedef std::function<double(std::function<double(double,double)>,bool,double,double)> RealFunc1Derivative;
+typedef std::function<double(std::function<double(double,double)>,double,double)> RealFunc1Derivative;
 
-std::function<double(double, double, double)> RealFunc2;
-typedef std::function<double(std::function<double(double,double,double)>,bool,double,double,double)> RealFunc2Derivative;
+std::function<double(double, vector<double>, vector<double>,int)> RealFunc2;
+typedef std::function<double(std::function<double(double,vector<double>,vector<double>,int)>,double,vector<double>,vector<double>,int)> RealFunc2Derivative;
 
 double Func1(double x, double gamma){
   return 0.5*pow(x,2)*(1+gamma*cos(x));
 }
 
-double Func2(double x, double y, double z){
-  return 0.5*pow(abs(z-tanh(x*y)),2)
-}
+double Func2(double x, vector<double> y, vector<double> z, int m){
+  double f;
+  for(int i=1;i<=m;i++){
+    f+=pow(abs(z[i]-tanh(x*y[i])),2);
+  }
+  return 0.5*f;
+};
 
-double computeDerivative1(RealFunc f, bool xEval, double x, double gamma){
+double computeDerivative1(RealFunc1 f, double x, double gamma){
   double h = sqrt(std::numeric_limits<double>::epsilon());
   return (f(x+h,gamma)-f(x,gamma))/h;  
 }
 
-double computeDerivative2(RealFunc f, bool xEval, double x, double y, double z){
+double computeDerivative2(RealFunc2 f, double x, vector<double> y, vector<double> z, int m){
   double h = sqrt(std::numeric_limits<double>::epsilon());
-  return (f(x+h,y,z)-f(x,y,z))/h;  
+  return (f(x+h,y,z,m)-f(x,y,z,m))/h;  
 }
 
-void SteepestDecentMethod(RealFunc f, RealFuncDerivative d, double x0, double y0, double gamma, double alpha, double precision){
-  double x = x0, y = y0;
-  bool xEval = true;
+void SteepestDecentMethod(RealFunc1 f1, RealFunc2 f2, RealFunc1Derivative d1, RealFunc2Derivative d2, double x0, double y0, double alpha, double beta, double gamma, double precision){
+  double xk = x0, xn = 0, xkm = x0;
+ 
+  chrono::time_point<std::chrono::system_clock> start, end;
+  chrono::duration<double> elapsed_seconds;
+  
+  start = chrono::system_clock::now();
   for(int i=0;;i++){
-    x=x-alpha*f(x,y,b)/d(f,xEval,x,y,b);
+    xn=xk-alpha*d(f1,xk,gamma)+beta*(xk-xkm);
+    xkm=xk;
+    xk=xn;
     std::cout << "Iteration: " << i << std::endl;
-    if(abs(f(x,y,b)<precision)){
+    if(abs(f(xn,gamma)<precision)){
+      end = chrono::system_clock::now();
+      elapsed_seconds = end-start;
       std::cout << "Function Local Minimum x: " << x << std::endl;
+      std::cout << "Total time was: " << elapsed_seconds.now() << endl;
+      break;
+    }
+  }
+  
+  xk=x0; xkm = x0;  
+  start = chrono::system_clock::now();
+  for(int i=0;;i++){
+    xn=xk-alpha*d(f2,xk,y,z,z.size())+beta*(xk-xkm);
+    xkm=xk;
+    xk=xn;
+    std::cout << "Iteration: " << i << std::endl;
+    if(abs(f(xn,y,z,z.size())<precision)){
+      end = chrono::system_clock::now();
+      elapsed_Seconds = end-start;
+      std::cout << "Function Local Minimum x: " << x << std::endl;
+      std::cout << "Total time was: " << elapsed_seconds.now() << endl;
       return;
     }
   }
+ 
 }
 
 int main(){
-  RealFunc f{Func};
-  RealFuncDerivative d{computeDerivative};
-  
-  double x0=5.0, y0=0.0, alpha=1.0, L=1;
-  
+  RealFunc f1{Func1};
+  RealFunc f2{Func2};
+  RealFunc1Derivative d1{computeDerivative2};
+  RealFunc2Derivative d2{computeDerivative2}
+ 
+  double x0 = 0, y0 = 0, alpha=1.0, beta = 0;
+  vector<double> y = {1.0,2.0,3.0,4.0,5.0}
+  vector<double> z = {1.0,1.0,1.0,1.0,1.0}
+ 
   std::cout << "Initial x: " << x0 << std::endl;
   std::cout << "Initial y: " << y0 << std::endl;
   std::cout << "Initial alpha: " << alpha << std::endl; 
-    
-  SteepestDecentMethod(f, d, x0, y0, 1.0, alpha, 10e-3);
+  std::cout << "Initial beta: " << beta << std::endl;
+  std::cout << "Initial gamma: " << gamma << std::endl;
   
-  std::cout << "Proving Lipschitz condition: " << std::endl;
-  std::cout << "||delf(x)-delf(y)|| = " << d(f,true,x0,y0,1.0)-d(f,false,x0,y0,1.0) << " <= L||x-y|| = " << x0-y0 <<  std::endl;
+  std::cout << "Descent for f(x)=x^2(1+gamma*cos(x))/2" << std::endl;
+  std::cout << "Descent for f(x)=sum_1_m*abs(z[i]-tanh(x*y[i]))^2/2" << std::endl;
   
+  std::cout << "Case #1: Traditional Steepest Descent with beta = 0" << std::endl;
+  SteepestDescentMethod(f1, f2, d1, d2, x0, y0, y, z, alpha, beta, gamma, 10e-3);
+  
+  std::cout << "Case #2: New Method with beta = 1" << std::endl;
+  SteepestDescentMethod(f1, f2, d1, d2, x0, y0, y, z, alpha, beta=1, gamma, 10e-3);
+
   return 0;
 }
