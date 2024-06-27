1b6f3736e69495447eed8d870f751ee1893e0ad4
milesmiles902 <milesmiles902@gmail.com>
2024-06-26 18:15:46 -0700

    Build time

---
+++ C and C++ Compiling/Chapter_06/windows.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <Windows.h>
+
+#ifndef __cplusplus
+extern "C" {
+  #endif
+  typedef HRESULT (*PDLL_REGISTER_SERVER)(void);
+  typedef HRESULT (*PDLL_UNREGISTER_SERVER)(void);
+  #ifdef __cplusplus
+}
+#endif
+
+enum{
+  CMD_LINE_ARG_INDEX_EXECUTABLE_NAME = 0,
+  CMD_LINE_ARG_INDEX_INPUT_DLL,
+  CMD_LINE_ARG_INDEX_REGISTER_OR_UNREGISTER,
+  NUMBER_OF_SUPPORTED_CMD_LINE_ARGUMENTS
+} CMD_LINE_ARG_INDEX
+
+int main(int argc, char *argv[]){
+  HINSTANCE dllHandle = ::LOADLIBRARYA(argv[CMD_LINE_ARG_INDEX_INPUT_DLL]);
+  if(NULL == dllHandle){
+    printf("Failed loading %s\n", argv[CMD_LINE_ARG_INDEX_INPUT_DLL]);
+    return -1;
+  }
+
+  if(NUMBER_OF_SUPPORTED_CMD_LINE_ARGUMENTS > argc){
+    PDLL_REGISTER_SERVER_pDLLRegisterServer = 
+      (PDLL_REGISTER_SERVER)GetProcAddress(dllHandle, "DllRegisterServer");
+    if(NULL == pDllRegisterServer)
+    {
+      printf("Failed finding the symbol \"DllRegisterServer\"");
+      ::FreeLibrary(dllHandle);
+      dllHandle = NULL;
+      return -1;
+  }
+  pDllRegisterServer();
+} else {
+  PDLL_UNREGISTER_SERVER pDllUnregisterServer = 
+    (PDLL_UNREGISTER_SERVER)GetProcAddress(dllHandle, "DllUnregisterServer");
+  if(NULL == pDllUnregisterServer){
+    printf("Failed finding the symbol \"DllUnregisterServer\"");
+    ::FreeLibrary(dllHandle);
+    dllHandle = NULL;
+    return -1;
+  }
+  pDllUnregisterServer();
+  }
+  ::FreeLibrary(dllHandle);
+  dllHandle = NULL;
+  return 0;
+}
+
+
